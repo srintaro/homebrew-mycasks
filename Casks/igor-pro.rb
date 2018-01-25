24cb1423c58fd2c7a80f8df4c1022aed66650439
srintaro <main@chem0846.chem.s.u-tokyo.ac.jp>
2018-01-25 10:53:05 +0900

    [Fix] Fix stylistic error on discrepancy on the url and homepage hostnames
    Added comment verifing the authenticity

--- Casks/igor-pro.rb
+++ Casks/igor-pro.rb
@@ -2,6 +2,7 @@ cask 'igor-pro' do
   version '7.0.7'
   sha256 '0d3fbea83ac6e1db0b7bb65644c2473526a03c5806575ae720b11f2976316dc7'
 
+  # wavemetrics.net was verified as official when first introduced to the cask
   url "http://www.wavemetrics.net/Downloads/Mac/Igor#{version.major}.dmg"
   name 'WaveMetrics Igor Pro'
   homepage 'https://www.wavemetrics.com/index.html'
