898241506dad31f54a3d1813b38d44ece7db2dbd
srintaro <mobileair@mab2015.local>
2019-09-18 23:11:20 +0900

    [Update] Update igor-pro to 8.0.3, fix sha256

--- Casks/igor-pro.rb
+++ Casks/igor-pro.rb
@@ -1,6 +1,6 @@
 cask 'igor-pro' do
   version '8.0.3'
-  sha256 '8993693b862bff51c23609d841aef40be8e1461d8e18951c872831195b5e750f'
+  sha256 'd7d5cb26b8f6dc77d9cc6e882add67316ccd1f2fd58b01214d9ae4d0134ba9b7'
 
   # wavemetrics.net was verified as official when first introduced to the cask
   url "http://www.wavemetrics.net/Downloads/Mac/Igor#{version.major}.dmg"
