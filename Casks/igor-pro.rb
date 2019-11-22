9918a76f8f110ca338a514dd0464be913af87d90
srintaro <main@chem0099.chem.s.u-tokyo.ac.jp>
2019-11-22 12:24:08 +0900

    [Update] Update igor-pro to 8.0.4

--- Casks/igor-pro.rb
+++ Casks/igor-pro.rb
@@ -1,6 +1,6 @@
 cask 'igor-pro' do
-  version '8.0.3'
-  sha256 'd7d5cb26b8f6dc77d9cc6e882add67316ccd1f2fd58b01214d9ae4d0134ba9b7'
+  version '8.0.4'
+  sha256 'c1a297865e61003b912f11a4c47dbbf19a590eb324e0cae0ef94598ebd233598'
 
   # wavemetrics.net was verified as official when first introduced to the cask
   url "http://www.wavemetrics.net/Downloads/Mac/Igor#{version.major}.dmg"
