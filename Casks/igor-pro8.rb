8f7b4167e02c226f7814e78ca99a1fc68cc534e7
srintaro <srintaro@users.noreply.github.com>
2021-09-09 09:17:24 +0900

    [Uodate] Update igor-pro to 9.0.0

--- Casks/igor-pro8.rb
+++ Casks/igor-pro8.rb
@@ -1,11 +1,11 @@
 cask 'igor-pro' do
-  version '9.0.0'
-  sha256 '69ce008e0eac23022189612193fa269e1a299a3276fad3973cabb6023df4d906'
+  version '8.0.4'
+  sha256 'c1a297865e61003b912f11a4c47dbbf19a590eb324e0cae0ef94598ebd233598'
 
   # wavemetrics.net was verified as official when first introduced to the cask
   url "http://www.wavemetrics.net/Downloads/Mac/Igor#{version.major}.dmg"
   name 'WaveMetrics Igor Pro'
   homepage 'https://www.wavemetrics.com/index.html'
 
-  suite 'Igor Pro 9 Folder'
+  suite 'Igor Pro 8 Folder'
 end
