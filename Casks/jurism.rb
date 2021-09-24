233d19382da8321effb7663b9431b186ac96fc6a
srintaro <srintaro@users.noreply.github.com>
2021-09-24 09:20:21 +0900

    [Add] Add jurism

---
+++ Casks/jurism.rb
@@ -0,0 +1,12 @@
+cask 'jurism' do
+  version '5.0.93m18'
+  sha256 '7d7ac4f1b7fe3f16947418c59cbe0985df0c84270fe416673b5148576c1e9640'
+
+  # wavemetrics.net was verified as official when first introduced to the cask
+  url "https://github.com/Juris-M/assets/releases/download/client%2Frelease%2F#{version.major_minor_patch}/Jurism-#{version.major_minor_patch}.dmg", verified: "github.com/Juris-M/"
+
+  name 'Jurism'
+  homepage 'https://juris-m.github.io/'
+
+  app 'Jurism.ap'
+end
